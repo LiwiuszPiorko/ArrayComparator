38177824324f8c1d04dff06fb78fc8ce9bb6f49f
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-10-03 10:49:16 +0200

    Kolejna wersja

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,20 +1,20 @@
 public class ArrayComparator {
 
-
-    int[] arrayX;
-    int[] arrayY;
+    private int[] arrayX;
+    private int [] arrayY;
 
     public ArrayComparator(int[] arrayX, int[] arrayY) {
         this.arrayX = arrayX;
         this.arrayY = arrayY;
     }
 
-
     boolean compareArrays() {
-        boolean same = arrayX == arrayY;
-        for()
 
-        if (same) {
+        for (int i = 0; i < arrayX.length; i++) {
+            for (int j = 0; j < arrayY.length; j++) ;
+        }
+
+        if (arrayX.length == arrayY.length) {
             return true;
         } else {
             return false;
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -3,23 +3,23 @@ import java.util.Arrays;
 public class ArrayTest {
 
     public static void main(String[] args) {
-        int[] tablica1 = {1, 3, 4, 9, 7, 0, 99};
-        int[] tablica2 = {1, 4, 88, 97, 0, 100};
+        int[] array1 = {1, 3, 4, 9, 7, 0};
+        int[] array2 = {1, 3, 4, 9, 7, 0,11};
 
-        String wtab1 = Arrays.toString(tablica1);
+        String wtab1 = Arrays.toString(array1);
         System.out.println(wtab1);
-        String wtab2 = Arrays.toString(tablica2);
+        String wtab2 = Arrays.toString(array2);
         System.out.println(wtab2);
 
-        ArrayComparator check = new ArrayComparator(tablica1, tablica2);
+        ArrayComparator check = new ArrayComparator(array1,array2);
         boolean ArrayComparator = check.compareArrays();
-
-        if (ArrayComparator) {
-            System.out.println("Tablice sa takie same");
+        if (true) {
+            System.out.print("Tablice są równe");
         } else {
-            System.out.println("Tablice nie sa takie same");
+            System.out.println("Tablice nie są równe");
         }
     }
-
-
 }
+
+
+
